405437e62bb0dbac11e685b0d908f5db14b5bf31
Victor88400 <kriegv@uwplatt.edu>
2022-09-08 12:18:48 -0500

    3.0 code changes
    Uses v2 APIs for 6.0 servers

--- src/main/java/flexagon/fd/plugin/jenkins/utils/PluginConstants.java
+++ src/main/java/flexagon/fd/plugin/jenkins/utils/PluginConstants.java
@@ -9,31 +9,46 @@ public class PluginConstants
 
 	//JSON Body Properties
 	public static final String CONTENT_TYPE_APP_JSON = "application/json";
-	public static final String JSON_AUTHENTICATION = "authentication";
-	public static final String JSON_USER_ID = "userId";
-	public static final String JSON_PASSWORD = "password";
 	public static final String JSON_WORKFLOW_REQUEST_ID = "workflowRequestId";
-	public static final String JSON_ENVIRONMENT_CODE = "environmentCode";
-	public static final String JSON_STREAM_NAME = "streamName";
-	public static final String JSON_RELEASE_NAME = "releaseName";
-	public static final String JSON_PROJECT_PATH = "qualifiedProjectName";
-	public static final String JSON_FORCE_BUILD = "forceBuild";
+	public static final String JSON_ENVIRONMENT_ID = "environmentId";
+	public static final String JSON_FORCE = "force";
+	public static final String JSON_PROJECT_ID = "projectId";
+	public static final String JSON_PROJECT_STREAM_ID = "projectStreamId";
+	public static final String JSON_PROJECT_BRANCH_ID = "projectBranchId";
+	public static final String JSON_RELEASE_DEF_ID = "releaseDefId";
+	public static final String JSON_WORKFLOW_OVERRIDE_ID = "workflowVersionOverrideId";
+	public static final String JSON_PACKAGE_NAME = "packageName";
+	public static final String JSON_FOLDER_ID = "folderId";
 
 	//URL Properties
-	public static final String URL_SUFFIX_WORKFLOW_STATUS = "/rest/workflow/getWorkflowRequestStatus";
-	public static final String URL_SUFFIX_BUILD_PROJECT = "/rest/workflow/buildProject";
-	public static final String URL_SUFFIX_BUILD_APPLICATION = "/rest/workflow/buildApplication";
+	public static final String URL_SUFFIX_WORKFLOW_STATUS = "/rest/v2/workflowrequest";
+	public static final String URL_SUFFIX_BUILD_PROJECT = "/rest/v2/workflowrequest/build";
+	public static final String URL_SUFFIX_SEARCH_ENVIRONMENT = "/rest/v2/topology/environment?environmentCode=";
+	public static final String URL_SUFFIX_GET_PROJECT = "/rest/v2/project";
+	public static final String URL_SUFFIX_SEARCH_PROJECT_1 = "?projectName=";
+	public static final String URL_SUFFIX_SEARCH_PROJECT_2 = "&folderId=";
+	public static final String URL_SUFFIX_SEARCH_BRANCH = "/branch?branchName=";
+	public static final String URL_SUFFIX_SEARCH_FOLDER_1 = "/rest/v2/folder?folderName=";
+	public static final String URL_SUFFIX_SEARCH_FOLDER_2 = "&parentFolderId=";
+	public static final String URL_SUFFIX_SEARCH_RELEASE = "/rest/v1/releases?releaseName=";
+    public static final String URL_SUFFIX_GET_WORKFLOW = "/rest/v2/workflow/";
+
+	//Flexdeploy Folder
+	public static final int FLEXDEPLOY_FOLDER_ID = 99;
 
 	//Workflow Status
-	public static final String WORKFLOW_STATUS_FAIL = "FAILED";
+	public static final String WORKFLOW_STATUS_FAILED = "FAILED";
 	public static final String WORKFLOW_STATUS_COMPLETED = "COMPLETED";
+	public static final String WORKFLOW_STATUS_ABORTED = "ABORTED";
+	public static final String WORKFLOW_STATUS_REJECTED= "REJECTED";
 
 	//Timeouts
 	public static final int TIMEOUT_WORKFLOW_EXECUTION = 15; //Minutes
 	public static final int TIMEOUT_CONNECTION_VALIDATION = 5000; //Millis
 
 	public static final String ERROR_LOGIN_FAILURE = "Login failure";
-	public static final String ERROR_ID_NOT_FOUND = "WorkflowRequestId [1] not found";
+	public static final String ERROR_ID_NOT_FOUND = "WorkflowRequest not found";
+	public static final String ERROR_NULL_POINTER = "java.lang.NullPointerException";
 
 	private PluginConstants()
 	{
